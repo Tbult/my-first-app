278b1f3dd227c7e5502a94e1881b9c5694600fbc
Tonke Bult <tonksol@gmail.com>
2019-09-25 20:58:51 +0200

    Toevoegen en maken van de server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'; // voeg hier de import van je component toe (typescript)
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent // voeg hier je component toe
   ],
   imports: [
-    BrowserModule
+    BrowserModule // laden van ingebouwde Angular modules 
   ],
   providers: [],
   bootstrap: [AppComponent]
