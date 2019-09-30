d3a1a5750dd345e6cdc311073190a9f684da2452
Tbult <55149824+Tbult@users.noreply.github.com>
2019-09-30 11:47:24 +0200

    Nest server component in nieuwe component servers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,18 @@ import { NgModule } from '@angular/core';
 // import { FormsModule} from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component'; // voeg hier de import van je component toe (typescript)
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component'; // voeg hier de import van je component toe (typescript)
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent // voeg hier je component toe
+    ServerComponent,
+    ServersComponent // voeg hier je component toe
   ],
   imports: [
-<<<<<<< HEAD
     BrowserModule
-    // FormsModule
-=======
-    BrowserModule // laden van ingebouwde Angular modules 
->>>>>>> 278b1f3dd227c7e5502a94e1881b9c5694600fbc
   ],
   providers: [],
   bootstrap: [AppComponent]
